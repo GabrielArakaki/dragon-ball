1b5c9e3df5b23d061af9b1f257a253c32acfc571
GabrielArakaki <arakaki.gabriel@gmail.com>
2017-08-27 23:26:48 -0300

    change dragonball script to promises

--- dragonball.js
+++ dragonball.js
@@ -2,49 +2,53 @@ const xray = require('x-ray')
 const R = require('ramda')
 const inquirer = require('inquirer')
 const childProcess = require('child_process')
+const Promise = require('bluebird')
 
 const x = xray()
 
 runProgram()
 
 function runProgram () {
-  selectAllEpisodes((err, episodes) => {
-    if (err) throw new Error('EPISODES NOT FOUND')
-  
-    const inquirerChoices = episodes.map(episode => {
-      return {
-        name: episode.title,
-        value: episode.url
-      }
-    })
-  
-    inquirer
-      .prompt([{
-        type: 'list',
-        name: 'episode',
-        message: 'Choose an episode:',
-        choices: inquirerChoices
-      }])
-      .then(value => value.episode)
-      .then(findEpisodeVideoURL)
-  })  
+  getAllEpisodes()
+    .then(getUserInput)
+    .then(getEpisodeVideoURL)
+    .done(openVideoInChrome)
 }
 
-function selectAllEpisodes(callback) {
+function openVideoInChrome(finalUrl){
+  childProcess.exec(`open -a "Google Chrome" ${finalUrl}`)
+}
+
+function getUserInput(episodes) {
+  return inquirer
+    .prompt([{
+      type: 'list',
+      name: 'episode',
+      message: 'Choose an episode:',
+      choices: episodes
+    }])
+    .then(value => value.episode)
+}
+
+function getAllEpisodes() {
   const scope = {
     url: ['li a@href'],
     title: ['li a'] 
   }
   
-  x('http://www.animesonlinebr.com.br/dublados/147', '.single', scope)(function(err, response) {
-    if (err) return callback(err)
-  
-    const episodes = transformTwoArraysIntoCollection(response.url, response.title, ['url', 'title'])
-
-    callback(null, episodes)
-  })  
-
-  setTimeout(function(){}, 10000)
+  const getEpisodes = Promise.promisify(x('http://www.animesonlinebr.com.br/dublados/147', '.single', scope))
+  return getEpisodes()
+    .then(response => {
+      return transformTwoArraysIntoCollection(response.url, response.title, ['url', 'title'])
+    })
+    .then(episodes => {
+      return episodes.map(episode => {
+        return {
+          name: episode.title,
+          value: episode.url
+        }
+      })
+    })
 }
 
 
@@ -59,26 +63,21 @@ function transformTwoArraysIntoCollection (array1, array2, names) {
     })
 }
 
-function findEpisodeVideoURL (baseURL) {
+function getEpisodeVideoURL (baseURL) {
   console.log('BASE URL', baseURL)
   const scope = {
     url: ['link@href'],
     type: ['link@itemprop']
   }
 
-  x(baseURL, '.contentBox', scope)(function(err, response){
-    
-    const finalIndex = R
-      .findIndex(item => item === 'embedURL')(response.type)
-    
-    const finalUrl = response.url[finalIndex]
+  const getVideoUrl = Promise.promisify(x(baseURL, '.contentBox', scope))
+  return getVideoUrl()
+    .then(response => {
+      const finalIndex = R
+        .findIndex(item => item === 'embedURL')(response.type)
 
-    console.log('finalUrl ', finalUrl)
-  
-    childProcess.exec(`open -a "Google Chrome" ${finalUrl}`)
-  })
-
-  setTimeout(function(){}, 10000)
+      return response.url[finalIndex]  
+    })
 }
 
 module.exports = {
