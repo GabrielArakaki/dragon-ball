2c32df6c1b23fdeead2d3d0e73b186fdede8c99b
Gabriel Arakaki <guybrush182@gmail.com>
2017-12-16 18:43:36 -0200

    moved folder to bower_components

--- client/gruntFile.js
+++ client/gruntFile.js
@@ -1,5 +1,31 @@
+const dotenv = require('dotenv')
+dotenv.config({ silent: true })
+
 module.exports = grunt => {
+  grunt.loadNpmTasks('grunt-contrib-connect')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-replace')
 
-  grunt.registerTask('buildjs', ['html2js','concat','replace:application','sass','copy:assets']);
+  grunt.registerTask('dev', ['connect:server', 'watch'])
 
+  grunt.initConfig({
+    connect: {
+      server  : {
+        options   : {
+          base        : 'www',
+          port        : 3000,
+          livereload  : true
+        }
+      }
+    },
+    watch:{
+      all: {
+        files:['www/app.js'],
+        tasks:['default','timestamp']
+      },
+    },
+    replace: {
+      app:
+
+  })
 }
---
+++ client/www/app.js
@@ -0,0 +1,70 @@
+'use strict'
+
+var OPTIONS = [
+  "Hello, AngularJs",
+  "Hello, Seu Viadão"
+]
+
+angular
+  .module('app', [
+    'ngSanitize',
+    'com.2fdevs.videogular',
+    'com.2fdevs.videogular.plugins.controls',
+    'com.2fdevs.videogular.plugins.overlayplay',
+    'com.2fdevs.videogular.plugins.poster',
+    'ui.bootstrap'
+  ])
+  .controller('HelloController', ['$sce', '$scope', '$http', function($sce, $scope, $http) {
+    var DOMAIN = 'http://localhost:9001/'
+    $scope.message = 'TESTE DO GABAO'
+    $scope.config = {
+      startTime: 120,
+      autoHide: false,
+      autoHideTime: 3000,
+      autoPlay: true,
+      sources: [],
+      theme: "bower_components/videogular-themes-default/videogular.css",
+      plugins: {
+        poster: "http://www.videogular.com/assets/images/videogular.png"
+      }
+    }
+
+    $scope.onSelectAnime = function ($item, $model, $label) {
+      $scope.animeresult = $item
+    };
+    
+    $scope.onSelectEpisode = function ($item, $model, $label) {
+      return $http
+        .get(DOMAIN + 'episode', 
+          { params: { baseUrl: $item.url } })
+        .then(function(response) {
+          $scope.config.sources = [
+            { 
+              src: $sce.trustAsResourceUrl(response.data),
+              type: 'video/mp4',
+            }
+          ]
+        })
+    };
+    
+    $scope.getAnime = function(val) {
+      return $http
+        .get(DOMAIN + 'animes', { params: { string : val } }) 
+        .then(function(response){
+          return response.data
+        });
+    };
+
+    $scope.getEpisode = function(val) {
+      return $http
+        .get(DOMAIN + 'episodes', 
+          { params: { animeUrl: $scope.animeresult.url, string: val } })
+        .then(function(response){
+          return response.data
+        })
+    }
+
+    $scope.onPlayerReady = function(API) {
+      $scope.API = API
+    }
+  }])
